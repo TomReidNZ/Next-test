daed7e7c9f4cf4924804676483e32bf8406bdcb1
Tom Reid (Spike Techniques) <v-thre@microsoft.com>
2021-05-29 17:26:38 +1200

    add in zip

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@
 subscription=$(az account list --query [0].id -o tsv)
 resourceGroupName=$(az group list --query "[0] | name" -o tsv)
 
+sourceZip=https://aiengineeringltdcom-my.sharepoint.com/:u:/g/personal/tom_aiengineeringltd_com/EVYxaecxItFPkJMLhXsQ1koBshG5ZZmLUGEPSuDDbaEuog?e=Zdd2vk
+curl $sourceZip --output source.zip
